690fa2b5463f6c532453a2503f0e21a3f3466b34
plato1111 <64042307+plato1111@users.noreply.github.com>
2024-05-30 19:37:45 -0400

    css

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const html = `
   <title>Twelve - The Company</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
-  <link rel="stylesheet" href="public/css/style.css">
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <div class="container d-flex flex-column justify-content-center align-items-center min-vh-100">
