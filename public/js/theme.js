b5089e13563273efa53e2ce9b48b7b54398ac467
plato1111 <64042307+plato1111@users.noreply.github.com>
2024-07-28 19:00:12 -0400

    Update theme.js

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,1 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const themeToggle = document.getElementById('theme-toggle');
+  const currentTheme = localStorage.getItem('theme');
 
+  if (currentTheme === 'dark') {
+    document.body.classList.add('dark-mode');
+    themeToggle.checked = true;
+  }
+
+  themeToggle.addEventListener('change', function() {
+    document.body.classList.toggle('dark-mode');
+    localStorage.setItem('theme', this.checked ? 'dark' : 'light');
+  });
+});
