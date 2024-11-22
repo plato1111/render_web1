cec795df6e0ed9f286a29b979305520d9eb8daae
plato1111 <64042307+plato1111@users.noreply.github.com>
2024-11-22 09:12:38 -0400

    Update app.js
    page2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3001;
 app.use(express.static('public'));
 
 app.get("/", (req, res) => res.type('html').send(html));
-app.get("/page2", (req, res) => res.type('html').send(page2));
+app.get("page2", (req, res) => res.type('html').send(page2));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
