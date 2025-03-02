5ce30d54bc444c5de108e15508e789312184b3c4
plato1111 <64042307+plato1111@users.noreply.github.com>
2025-03-01 20:28:53 -0400

    Update app.js
    tittle change

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const html = `
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Twelve - The Company</title>
+  <title>Pathfinder Pro - Orientación Vocacional | Puerto Rico</title>
   <link rel="stylesheet" href="/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
